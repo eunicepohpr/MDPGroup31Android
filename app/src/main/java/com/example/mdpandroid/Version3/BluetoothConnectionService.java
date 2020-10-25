bea4b93ac5098ef30547ee25e2257a258d66ccf9
Eunice <eunicepoh98@gmail.com>
2020-10-25 15:37:44 +0800

    migrate to androidx

--- app/src/main/java/com/example/mdpandroid/Version3/BluetoothConnectionService.java
+++ app/src/main/java/com/example/mdpandroid/Version3/BluetoothConnectionService.java
@@ -7,7 +7,7 @@ import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.content.LocalBroadcastManager;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.util.Log;
 import android.widget.Toast;
 
